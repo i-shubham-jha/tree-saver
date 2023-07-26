579bff522bcfd1f58771963f694f3f96025c5613
shubham <>
2023-07-27 01:51:07 +0530

    concise demo in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,44 @@
 #include "tree-saver.h"
 
-struct Node
+struct node
 {
-    int data;
-    Node * left;
-    Node * right;
+    int data1;
+    float data2;
+    node * left, * right;
 };
+
+node * createNode(int a, double b)
+{
+    node * t = new node;
+    t->data1 = a;
+    t->data2 = b;
+    t->left = t->right = NULL;
+    return t;
+}
 
 int main ()
 {
-    Node * root = new Node;
-    root->right = NULL;
-    root->data = 0;
+    // this is just a simple demo
 
-    Node * n2 = new Node;
-    n2->left = NULL;
-    n2->right = NULL;
-    n2->data = 0;
+    node * root = createNode(2, 5.282);
 
-    root->left = n2;
+    root->left = createNode(5, 40.162 );
 
-    TreeSaver<Node> ob;
+    root->right = createNode(676, 3838.873 );
 
-    ob.save(root);
-    puts("Jinda hai");
+    TreeSaver<node> obj1;
+    obj1.save(root);
+
+    free(root->left);
+    free(root->right);
+    free(root);
+
+    TreeRetriever<node> obj2;
+    root = obj2.retrieve();
+
+    printf("root: %d, %f\n", root->data1, root->data2);
+    node * temp = root->left;
+    root = root->right;
+    printf("LC: %d, %f\n", temp->data1, temp->data2);
+    printf("root: %d, %f\n", root->data1, root->data2);
 }
